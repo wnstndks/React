85ff0f2f92e089ace5b9225b7edb9b7b42097e45
junsu <anjs9912@gmail.com>
2024-03-06 17:52:03 +0900

    컴포넌트 여러개

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,3 +1,5 @@
+// 원인 메세지 없애기
+// eslint-disable-next-line
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -5,6 +7,7 @@ import { Button, Container, Navbar, Nav, Col, Row } from "react-bootstrap";
 import { useState } from "react";
 // 경로는 무조건 ./ 부터
 import data from "./data.js";
+
 
 function App() {
   // 길고 복잡한 데이터들은 다른 js파일에 빼둘수 있음
@@ -26,25 +29,12 @@ function App() {
       <div className="main-bg"></div>
       <Container>
         <Row>
-        <Modal shoes={shoes}/>
-          <Col>
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-            />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </Col>
-          <Col>
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-            />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </Col>
+          <Modal shoes={shoes}/>
+          <Modal2 shoes={shoes}/>
+          <Modal3 shoes={shoes}/>
         </Row>
       </Container>
+
     </div>
   );
 }
@@ -55,6 +45,24 @@ function Modal(props) {
       <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
       <h4>{props.shoes[0].title}</h4>
       <p>{props.shoes[0].price}</p>
+    </Col>
+  );
+}
+function Modal2(props) {
+  return (
+    <Col>
+      <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+      <h4>{props.shoes[1].title}</h4>
+      <p>{props.shoes[1].price}</p>
+    </Col>
+  );
+}
+function Modal3(props) {
+  return (
+    <Col>
+      <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+      <h4>{props.shoes[2].title}</h4>
+      <p>{props.shoes[2].price}</p>
     </Col>
   );
 }
