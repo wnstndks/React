1647dae489457ccf163885fedeaa621d2e843210
junsu <anjs9912@gmail.com>
2024-03-07 22:39:46 +0900

    Router

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,10 +5,16 @@ import { Button, Container, Navbar, Nav, Col, Row } from "react-bootstrap";
 import { useState } from "react";
 // 경로는 무조건 ./ 부터
 import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   // 길고 복잡한 데이터들은 다른 js파일에 빼둘수 있음
   let [shoes] = useState(data);
+
+  // 페이지 나누는 법(리액트)
+  // 컴포넌트 만들어서 상세페이지 내용 채움
+  // 누가/detail 접속하면 그 컴포넌트 보여줌
+  // react-router-dom 라이브러리 쓰기(라우팅)
 
   return (
     <div className="App">
@@ -16,30 +22,59 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Link to="/">Home</Link>
+            <Link to="/detail">Detail</Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {shoes.map(function (a, i) {
-            return (
-              <Col>
-                <img
-                  src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
-                  width="80%"
-                />
-                <h4>{a.title}</h4>
-                <p>{a.price}</p>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+
+      {/* 페이지 이동버튼은 <Link> */}
+
+      <Routes>
+        {/* 라우트라는 컴포넌트는 페이지를 의미 */}
+        {/* 페이지또한 컴포넌트로 만들면 좋음 */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map(function (a, i) {
+                    return (
+                      <Col>
+                        <img
+                          src={`https://codingapple1.github.io/shop/shoes${
+                            i + 1
+                          }.jpg`}
+                          width="80%"
+                        />
+                        <h4>{a.title}</h4>
+                        <p>{a.price}</p>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Container>
+            </div>
+          }
+        />
+        {/* 누가 /로 접속할때 element안에 들어있는걸 보여줌 */}
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+        {/* 누가 /detail로 접속할때 element안에 들어있는걸 보여줌 */}
+      </Routes>
     </div>
   );
 }
+
+// function Card(props) {
+//   return (
+//     <Col>
+//       <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+//       <h4>{props.shoes[0].title}</h4>
+//       <p>{props.shoes[0].price}</p>
+//     </Col>
+//   );
+// }
 
 export default App;
--- shop/src/index.js
+++ shop/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// ./ 내가 만든 js파일들은 ./
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// react router
+// 경로가 없는것들은 라이브러리
+import {BrowserRouter} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
