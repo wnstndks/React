d2656eb545d5d77e0f7fc5b764f59c091b81b9a0
junsu <anjs9912@gmail.com>
2024-03-09 23:01:10 +0900

    lifecycle

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,22 +1,58 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 /* classname들을 styled-compoents 쓰면 JS파일에서 전부해결가능 */
-let YellowBtn = styled.button`
-  background: yellow;
-  color: black;
-  padding: 10px;
-`;
+// let YellowBtn = styled.button`
+//   background: yellow;
+//   color: black;
+//   padding: 10px;
+// `;
 
-let Box = styled.div`
-  background: grey;
-  paddding: 20px;
-`;
+// let Box = styled.div`
+//   background: grey;
+//   paddding: 20px;
+// `;
+
+// 컴포넌트의 Lifecycle 
+//  페이지에 장착되기도 하고(mount) 
+//  가끔 업데이트도 되고(update) 필요없으면 제거되고 (unmount)
+// class Detail2 extends React.Component{
+//   componentDidMount(){
+    
+//   }
+//   componentDidUpdate(){
+    
+//   }
+//   componentWillUnmount(){
+    
+//   }
+// }
 
 // App->detail 컴포넌트에 데이터 전송
 function DetailCard(props) {
+
+
+  // 컴포넌트에 갈고리 다는법 
+  // useEffect 안에 있는 코드는 나중에 실행이됨
+  useEffect(()=>{
+    // 업데이트는 재랜더링과 똑같은 말
+    // 여기적은 코드는 컴포넌트 로드 & 업데이트마다 실행됨
+    // mount update 시 코드 실행
+    console.log('안녕')
+  })
+
+
+  for (var i=0; i<1000;i++){
+    console.log(1);
+
+  }
+  let [count, setCount] = useState(0)
+
+
+
   // 유저가 URL파라미터에 입력한거 가져오려면 useParams()
   // 유저가 :id 자리에 적은거 가져와줌
   let { id } = useParams();
@@ -30,9 +66,11 @@ function DetailCard(props) {
   // URL 파라미터에 이상한거 입력하면? - if문 사용
   return (
     <Container>
-      <Box>
+      {count}
+      <button onClick={()=>{setCount(count+1)}}>버튼</button>
+      {/* <Box>
         <YellowBtn>버튼</YellowBtn>
-      </Box>
+      </Box> */}
       <Row>
         <div className="col-md-6">
           <img
