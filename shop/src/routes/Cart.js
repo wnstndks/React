0a1cdc1c0fc82ad69ee083ec6cdf11039f71782c
junsu <anjs9912@gmail.com>
2024-03-13 15:42:06 +0900

    Redux state

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,6 +1,17 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Cart() {
+  // store에 있던 componente가져다쓰기
+  let state = useSelector((state) => {
+    return state.data;
+  });
+  console.log(state);
+
+  // useSelector 편하게 쓰려면 함수를 store안에 있던 모든 state가
+  // 되기에, 어떤 state만 가져다 쓸지를 확실히 해야함 {}안에 원하는 것만 골라서 가져다 쓸수 있음
+
+  // 컴포넌트간 공유가 필요없으면 그냥 useState() 쓰는게 좋음
   return (
     // Redux사용시 컴포넌트들이 props없이 state공유가능
     <div>
@@ -16,10 +27,14 @@ function Cart() {
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
+            <td>{state[0].id}</td>
+            <td>{state[0].name}</td>
+            <td>{state[0].count}</td>
+          </tr>
+          <tr>
+            <td>{state[1].id}</td>
+            <td>{state[1].name}</td>
+            <td>{state[1].count}</td>
           </tr>
         </tbody>
       </Table>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,8 +1,41 @@
 // Redux 세팅시 store.js 파일을 생성해야 함
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+
+// Redux쓰는 이유 - 컴포넌트간 state 공유 편해짐 - props 전송이 편해짐
+
+// Redux store에 store 보관하는 법
+let user = createSlice({
+  // useState와 비슷한 역할
+  // name:'state이름',
+  // initialState : '실제state의값'
+
+  name:'user',
+  initialState:'kim'
+
+})
+
+let stock = createSlice({
+  name:'stock',
+  initialState:[10,11,12]
+
+})
+
+let data = createSlice({
+  name:'data',
+  initialState : [
+    {id : 0, name : 'White and Black', count : 2},
+    {id : 2, name : 'Grey Yordan', count : 1}
+  ] 
+})
+
 
 export default configureStore({
   reducer: { 
-    
+    // 여기에 등록해야 사용가능
+    user : user.reducer,
+    stock : stock.reducer,
+    data : data.reducer,
+
   }
 }) 
