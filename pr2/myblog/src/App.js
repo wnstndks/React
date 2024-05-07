59403cf99748262912fa8e6579a29b55fa71449a
wnstndks <anjs9912@gmail.com>
2024-05-07 22:48:59 +0900

    useState

--- pr2/myblog/src/App.js
+++ pr2/myblog/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import {useState} from 'react';
+import "./App.css";
 
 function App() {
+  // 변수를 html에 꽂아넣을 때는 중괄호
+  let data = "강남 우동 맛집";
+  let [a,b]=useState('남자 코트 추천');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 블로그</div>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
