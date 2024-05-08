0449e6753026635f3a6817672ca5d0dcb4c4ba77
wnstndks <anjs9912@gmail.com>
2024-05-08 23:16:53 +0900

    useState 활용

--- pr2/myblog/src/App.js
+++ pr2/myblog/src/App.js
@@ -7,7 +7,7 @@ function App() {
   let data = "강남 우동 맛집";
 
   // useState('보관할 자료')_state에 자료를 잠깐 저장할 수 있음
-  let [글제목, b] = useState([
+  let [글제목, 글제목변경]=useState([
     "남자 코트 추천",
     "강남 우동맛집",
     "파이썬 독학",
@@ -15,17 +15,28 @@ function App() {
 
   let [좋아요,좋아요변경]=useState(0);
 
+  
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 블로그</div>
+        <button className="bt-name" onClick={()=>{
+          let copy=[... 글제목];
+          copy.sort()
+          글제목변경(copy)
+        }}>가나다순 정렬 버튼</button>
       </div>
       <div className="list">
         <h4>
           {글제목[0]}
           <br />
-          <span style={{'cursor':'pointer'}}onClick={()=>{좋아요변경(좋아요+1)}}>👍</span> {좋아요}
-          <button className="bt-name">이름변경</button>
+          <span style={{'cursor':'pointer'}} onClick={()=>{좋아요변경(좋아요+1)}}>👍</span> {좋아요}
+          <button className="bt-name" onClick={()=>{
+            // array, object 자료를 다룰 때는 원본 데이터를 직접 조작하는 것보다는 기존값은 보존해주는 식으로 코드짜는게 좋은 관습
+            let copy= [...글제목];
+            copy[0]='여자 코트 추천';
+            글제목변경(copy)
+          }}>이름변경</button>
         </h4>
         <p>2월 17일 발행</p>
         <hr />
@@ -40,6 +51,7 @@ function App() {
         <p>2월 17일 발행</p>
         <hr />
       </div>
+
     </div>
   );
 }
