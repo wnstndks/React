b46e4d32f75e48ade441e20dd020a9b09ff97548
wnstndks <anjs9912@gmail.com>
2024-05-10 00:18:19 +0900

    input

--- pr2/myblog/src/App.js
+++ pr2/myblog/src/App.js
@@ -13,18 +13,20 @@ function App() {
     "파이썬 독학",
   ]);
 
-  let [글제목2,글제목변경2]= useState([
-      "나의 코트 추천",
-      "강남 라면 맛집",
-      "REACT 독학",
-    ]
-  );
+  let [글제목2, 글제목변경2] = useState([
+    "나의 코트 추천",
+    "강남 라면 맛집",
+    "REACT 독학",
+  ]);
 
-  let [title,setTitle]=useState(0);
+  let [title, setTitle] = useState(0);
 
-  let [좋아요, 좋아요변경] = useState([0,0,0]);
+  let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [count, setCount] = useState(1);
+
+  let [입력값, 입력값변경] = useState("");
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -40,7 +42,7 @@ function App() {
           가나다순 정렬 버튼
         </button>
       </div>
-      {글제목.map((a,i) => {
+      {글제목.map((a, i) => {
         return (
           <div className="list" key={i}>
             <h4>
@@ -58,8 +60,8 @@ function App() {
               <span
                 className="mycursor"
                 onClick={() => {
-                  let copy=[...좋아요];
-                  copy[i]+=1
+                  let copy = [...좋아요];
+                  copy[i] += 1;
                   좋아요변경(copy);
                 }}
               >
@@ -71,7 +73,7 @@ function App() {
                 onClick={() => {
                   // array, object 자료를 다룰 때는 원본 데이터를 직접 조작하는 것보다는 기존값은 보존해주는 식으로 코드짜는게 좋은 관습
                   let copy = [...글제목];
-                  let copy2 =[...글제목2];
+                  let copy2 = [...글제목2];
                   copy[i] = copy2[i];
                   글제목변경(copy);
                 }}
@@ -80,11 +82,28 @@ function App() {
               </button>
             </h4>
             <p>2월 17일 발행</p>
+            <button className="bt-name2" onClick={()=>{
+              let copy=[... 글제목];
+              copy.splice(i,1);
+              글제목변경(copy);
+            }}>삭제</button>
             <hr />
           </div>
         );
       })}
       {modal == true ? <Modal title={title} 글제목={글제목}></Modal> : null}
+      <div className="input_tag">
+          <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+            console.log(입력값);
+          }}
+        /><button className="bt-name" onClick={(e)=>{
+          let copy=[... 글제목];
+          copy.unshift(입력값);
+          글제목변경(copy)
+        }}>글발행</button>
+      </div>
     </div>
   );
 }
