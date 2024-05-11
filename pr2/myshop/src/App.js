45c2c036579354879c8d460eb414a242120b7d2a
wnstndks <anjs9912@gmail.com>
2024-05-11 19:31:17 +0900

    making modal

--- pr2/myshop/src/App.js
+++ pr2/myshop/src/App.js
@@ -3,50 +3,50 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import { useState } from "react";
-import data from './data';
+import data from "./data";
 
 function App() {
-  let [shoes]=useState(data);
-
+  let [shoes] = useState(data);
 
   return (
     <div className="App">
       <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">MyMusinsa</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login"></a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container>
+          <Navbar.Brand href="#home">MyMusinsa</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login"></a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div>
-        <div
-          className="main-bg"></div>
+        <div className="main-bg"></div>
       </div>
       <div className="container container2">
         <div className="row">
           <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
             <h4>{shoes[0].title}</h4>
-            <p>{ shoes[0].price }</p>
+            <p>{shoes[0].price}</p>
           </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{ shoes[1].price }</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{ shoes[2].price }</p>
-          </div>
+          <Modal shoes={shoes}></Modal>
+          <Modal shoes={shoes}></Modal>
+          <Modal shoes={shoes}></Modal>
         </div>
       </div>
     </div>
   );
 }
-
+function Modal(props) {
+  <div className="col-md-4">
+    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.price}</p>
+  </div>;
+}
 export default App;
