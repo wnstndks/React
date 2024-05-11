c0a82c7d2324b794bf48f18f916b0bfd56054454
wnstndks <anjs9912@gmail.com>
2024-05-11 19:26:59 +0900

    import data

--- pr2/myshop/src/App.js
+++ pr2/myshop/src/App.js
@@ -2,14 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { useState } from "react";
+import data from './data';
 
 function App() {
+  let [shoes]=useState(data);
+
+
   return (
     <div className="App">
-      <div>
-        <div
-          className="main-bg"></div>
-      </div>
       <Navbar className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">MyMusinsa</Navbar.Brand>
@@ -21,22 +22,26 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+      <div>
+        <div
+          className="main-bg"></div>
+      </div>
       <div className="container container2">
         <div className="row">
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{ shoes[0].price }</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{ shoes[1].price }</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{ shoes[2].price }</p>
           </div>
         </div>
       </div>
