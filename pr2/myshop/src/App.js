9f38b05a39460c840b2fcf25d94480e2f2920c6f
wnstndks <anjs9912@gmail.com>
2024-05-12 23:09:32 +0900

    design

--- pr2/myshop/src/App.js
+++ pr2/myshop/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { NavDropdown, Navbar, Container, Nav } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data";
 
@@ -11,17 +11,29 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">MyMusinsa</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Signed in as: <a href="#login"></a>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">MyMusinsa</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">profile</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <div>
         <div className="main-bg"></div>
       </div>
@@ -36,6 +48,7 @@ function App() {
           }
         </div>
       </div>
+      <div style={{"height" : "500px"}}></div>
     </div>
   );
 }
@@ -43,7 +56,7 @@ function App() {
 function Modal(props) {
   return(
   <div className="col-md-4">
-    <img src={`https://codingapple1.github.io/shop/shoes${props.title+1}.jpg`} width="80%" />
+    <img src={`https://codingapple1.github.io/shop/shoes${props.title+1}.jpg`} width="100%" />
     <h4>{props.shoes[props.title].title}</h4>
     <p>{props.shoes[props.title].price}</p>
   </div>
