e98bad061ae0b610c01a389833b671ecf4263e16
wnstndks <anjs9912@gmail.com>
2024-05-14 22:14:44 +0900

    useEffect

--- pr2/myshop/src/routes/Detail.js
+++ pr2/myshop/src/routes/Detail.js
@@ -1,18 +1,30 @@
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 import styles from "./Detail.module.css";
+import {useState, useEffect} from 'react';
 
 function Detail(props) {
+
+  let [alert,setAlert]=useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>{setAlert(false)},2000)
+  },[]);
+
   let { id } = useParams();
 
   let 찾은상품 = props.shoes.find((x) => {
     return x.id == id;
   });
 
-  console.log(id);
 
   return (
     <div className="container">
+      {
+        alert==true?<div className="alert alert-warning">
+          2초 이내 구매시 할인          
+        </div>:null
+      }
       <div className="row">
         <div className="col-md-6">
           <img
