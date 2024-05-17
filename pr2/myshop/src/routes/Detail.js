c0c733c13bdb19e8b2a4d57f2f1d5924ef45eb5d
wnstndks <anjs9912@gmail.com>
2024-05-17 22:13:29 +0900

    tab

--- pr2/myshop/src/routes/Detail.js
+++ pr2/myshop/src/routes/Detail.js
@@ -1,15 +1,18 @@
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
+import { NavDropdown, Navbar, Container, Nav } from "react-bootstrap";
+import styled from "styled-components";
 import styles from "./Detail.module.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 function Detail(props) {
+  let [alert, setAlert] = useState(true);
+  let [탭, 탭변경] = useState(0);
 
-  let [alert,setAlert]=useState(true)
-
-  useEffect(()=>{
-    setTimeout(()=>{setAlert(false)},2000)
-  },[]);
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, []);
 
   let { id } = useParams();
 
@@ -17,19 +20,19 @@ function Detail(props) {
     return x.id == id;
   });
 
-
   return (
     <div className="container">
-      {
-        alert==true?<div className="alert alert-warning">
-          2초 이내 구매시 할인          
-        </div>:null
-      }
+      {alert == true ? (
+        <div className="alert alert-warning">2초 이내 구매시 할인</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
-            src={`https://codingapple1.github.io/shop/shoes${찾은상품.id+1}.jpg`}
-            width="100%" className={`${styles.shoesimage}`}
+            src={`https://codingapple1.github.io/shop/shoes${
+              찾은상품.id + 1
+            }.jpg`}
+            width="100%"
+            className={`${styles.shoesimage}`}
           />
         </div>
         <div className={`col-md-6 ${styles.mybox}`}>
@@ -38,11 +41,57 @@ function Detail(props) {
           <p>{찾은상품.description}</p>
           <p>{찾은상품.price} 원</p>
         </div>
-        <button className={`btn btn-danger ${styles.orderbutton}`}>주문하기</button>
-        
+        <button className={`btn btn-danger ${styles.orderbutton}`}>
+          주문하기
+        </button>
       </div>
+      <div style={{ height: "100px" }} />
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(0);
+            }}
+            eventKey="link0"
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(1);
+            }}
+            eventKey="link1"
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              탭변경(2);
+            }}
+            eventKey="link2"
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent 탭={탭} />
     </div>
   );
 }
-
+function TabContent(props) {
+  if (props.탭 === 0) {
+    return <div>내용0</div>;
+  }
+  if (props.탭 === 1) {
+    return <div>내용1</div>;
+  }
+  if (props.탭 === 2) {
+    return <div>내용2</div>;
+  }
+}
 export default Detail;
