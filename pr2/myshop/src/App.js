6ee39d4d8a4c2d055c926fbfd398353e5107a13e
wnstndks <anjs9912@gmail.com>
2024-06-26 16:23:47 +0900

    정렬 및 추가 수정완

--- pr2/myshop/src/App.js
+++ pr2/myshop/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavDropdown, Navbar, Container, Nav } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import data from "./data.js";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail.js";
 import About from "./routes/About.js";
 import Musinsa from "./routes/Musinsa.js";
@@ -14,7 +13,7 @@ import axios from "axios";
 function App() {
   let [shoes, setShoes] = useState(data);
   let [count, setCount] = useState(1);
-  let [button, setButton] = useState(false);
+  let [mybutton, setMybutton] = useState(false);
 
   return (
     <div className={`${styles.App}`}>
@@ -59,41 +58,42 @@ function App() {
               </div>
               <button
                 onClick={() => {
-                  console.log(shoes)
-                  let copy=[...shoes.title]
-                  console.log(copy)
-
+                  try {
+                    let copy = [...shoes];
+                    copy.sort((a, b) => a.title.localeCompare(b.title));
+                    setShoes(copy);
+                  } catch (e) {
+                    console.log("실패함");
+                    console.log(e);
+                  }
                 }}
               >
                 상품명 정렬하기
               </button>
+
               <button
                 className={`${styles.mybutton}`}
+                style={{ display: mybutton ? "none" : "block" }}
                 onClick={() => {
                   setCount(count + 1);
                   console.log(count);
-                  {
-                    /* html에서 public 폴더 이미지 사용할땐 그냥/이미지경로 */
-                    /* 나중에 사이트를 발행하고 싶을 때는 문제 없지만, 서브 경로에다 발행시에는 문제가 생길 수 있음.. */
-                    /* 그래서 경로를 수정해야할수도 
-                    따라서 밑의 경로처럼 쓰기 */
-                    // {process.env.PUBLIC_URL+'/이미지 경로'}
-                  }
-                  if (count == 1) {
+
+                  if (count === 1) {
                     axios
                       .get("https://codingapple1.github.io/shop/data2.json")
                       .then((결과) => {
+                        console.log(결과.data);
                         let copy = [...shoes, ...결과.data];
                         setShoes(copy);
                       })
                       .catch(() => {
                         console.log("실패함");
                       });
-                  } else if (count == 2) {
+                  } else if (count === 2) {
                     axios
                       .get("https://codingapple1.github.io/shop/data3.json")
                       .then((결과2) => {
-                        setButton(true);
+                        setMybutton(true);
                         let copy = [...shoes, ...결과2.data];
                         setShoes(copy);
                       })
@@ -113,10 +113,7 @@ function App() {
 
         <Route path="/detail/:id" element={<Detail shoes={shoes} />}></Route>
         <Route path="/event" element={<About />}>
-          <Route
-            path="one"
-            element={<Musinsa element={(shoes = { shoes })} />}
-          />
+          <Route path="one" element={<Musinsa shoes={shoes} />} />
           <Route path="two" element={<div>회원가입 감사 쿠폰받기</div>} />
         </Route>
         <Route path="/cart" element={<Cart />} />
@@ -128,7 +125,7 @@ function App() {
 
 function Modal(props) {
   const titleIndex = Number(props.title);
-
+  
   return (
     <div className="col-md-4">
       <a href={`/detail/${titleIndex}`}>
@@ -137,6 +134,7 @@ function Modal(props) {
           width="100%"
         />
       </a>
+      <h4>{props.shoes[titleIndex].id}</h4>
       <h4>{props.shoes[titleIndex].title}</h4>
       <p>{props.shoes[titleIndex].price}</p>
     </div>
